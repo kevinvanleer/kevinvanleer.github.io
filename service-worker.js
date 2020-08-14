7b11f6dcc49d1f4a838d97b0b525b2b7ebdac303
Kevin Van Leer <kevin.vanleer@gmail.com>
2020-08-13 19:36:08 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e473ce7f937be460f7d12efaee9cf97.js"
+  "/precache-manifest.615f813f243c2c6b091bb33517d7559f.js"
 );
 
 self.addEventListener('message', (event) => {
