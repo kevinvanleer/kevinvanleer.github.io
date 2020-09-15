0a571dbcee615f6227787beb18767eec771e7b83
Kevin Van Leer <kevin.vanleer@gmail.com>
2020-09-15 09:21:42 -0500

    Updated styling and added thumbnails to links

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 import {
   faGithub,
   faLinkedin,
+  faReddit,
   faFacebook,
   faTwitter,
   faSoundcloud,
@@ -61,9 +62,9 @@ const App = () => {
           />
           <Title color="#ef8f00">kevinvanleer.com</Title>
         </Flexbox>
-        <Spacer height="2em" />
+        <Spacer height="5em" />
         <main tabIndex="0">
-          <Text>
+          <Text fontSize="label">
             Hi, I’m Kevin Van Leer, husband, father of three, and
             do-it-yourselfer. I love working with my hands to create beautiful
             and useful things. I’m just as passionate about creating enjoyable
@@ -72,7 +73,7 @@ const App = () => {
             to fruition.
           </Text>
           <Spacer height="1em" />
-          <Text>
+          <Text fontSize="label">
             I’ve been writing software for {moment('2004-06-12').fromNow(true)}{' '}
             and leading software teams for{' '}
             {moment('2008').fromNow(true).split(' ')[0]}. I’m passionate about
@@ -86,9 +87,11 @@ const App = () => {
             the best way to gain the respect and trust of those around you.
           </Text>
         </main>
+        <Spacer height="5em" />
+        <Text centerAlign fontSize="2em" color="#38c9e4">
+          Some of my projects
+        </Text>
         <Spacer height="2em" />
-        <Text fontSize="heading">Links</Text>
-        <Spacer height="1em" />
         <nav>
           <Flexbox
             justifyContent="space-around"
@@ -96,76 +99,112 @@ const App = () => {
             marginBetween="1em"
             wrapMargin="1em"
           >
-            <Link href="/resume">
+            <Link color="#84878e" href="http://covid19.kevinvanleer.com">
               <Flexbox flexDirection="column" alignItems="center">
-                <Icon size="10x" icon={faFile} />
-                <Spacer height="1em" />
-                <Text fontSize="label">Resume</Text>
-              </Flexbox>
-            </Link>
-            <Link href="http://covid19.kevinvanleer.com">
-              <Flexbox flexDirection="column" alignItems="center">
-                <Icon size="10x" icon={faMapMarkedAlt} />
+                <img
+                  style={{ border: '3px solid #5f6167', borderRadius: '10px' }}
+                  height="160px"
+                  src="/img/covid19-app-thumbnail.jpg"
+                />
                 <Spacer height="1em" />
                 <Text fontSize="label">COVID-19</Text>
                 <Text fontSize="label">Visualization</Text>
               </Flexbox>
             </Link>
-            <Link href="https://photos.app.goo.gl/ZEwJhPfaTAxpCdED8">
+            <Link
+              color="#84878e"
+              href="https://photos.app.goo.gl/ZEwJhPfaTAxpCdED8"
+            >
               <Flexbox flexDirection="column" alignItems="center">
-                <Icon size="10x" icon={faImages} />
+                <img
+                  style={{ border: '3px solid #5f6167', borderRadius: '10px' }}
+                  height="160px"
+                  src="/img/woodworking-thumbnail.jpg"
+                />
                 <Spacer height="1em" />
                 <Text fontSize="label">Woodworking</Text>
               </Flexbox>
             </Link>
+            <Link color="#84878e" href="/resume">
+              <Flexbox flexDirection="column" alignItems="center">
+                <img
+                  style={{ border: '3px solid #5f6167', borderRadius: '10px' }}
+                  height="160px"
+                  src="/img/resume-thumbnail.jpg"
+                />
+                <Spacer height="1em" />
+                <Text fontSize="label">Resume</Text>
+              </Flexbox>
+            </Link>
           </Flexbox>
         </nav>
-        <Spacer flexGrow={1} height="3em" />
+        <Spacer flexGrow={1} height="10em" />
+        <Text centerAlign fontSize="2em" color="#38c9e4">
+          More about me
+        </Text>
+        <Spacer height="2em" />
         <nav>
           <Flexbox
             marginBetween="1em"
             flexWrap="wrap"
             maxHeight="55px"
             overflow="hidden"
+            justifyContent="center"
           >
-            <Link href="https://github.com/kevinvanleer/">
+            <Link color="#84878e" href="https://github.com/kevinvanleer/">
               <Flexbox flexDirection="column" alignItems="center">
-                <Icon size="2x" icon={faGithub} />
+                <Icon color="#fff" size="2x" icon={faGithub} />
                 <Spacer height="0.5em" />
                 <Text fontSize="detail">GitHub</Text>
               </Flexbox>
             </Link>
-            <Link href="https://www.linkedin.com/in/kevin-vanleer/">
+            <Link color="#84878e" href="https://www.reddit.com/user/iamkvl/">
               <Flexbox flexDirection="column" alignItems="center">
-                <Icon size="2x" icon={faLinkedin} />
+                <Icon size="2x" color="#ff4500" icon={faReddit} />
+                <Spacer height="0.5em" />
+                <Text fontSize="detail">Reddit</Text>
+              </Flexbox>
+            </Link>
+            <Link
+              color="#84878e"
+              href="https://www.linkedin.com/in/kevin-vanleer/"
+            >
+              <Flexbox flexDirection="column" alignItems="center">
+                <Icon size="2x" color="#0077b5" icon={faLinkedin} />
                 <Spacer height="0.5em" />
                 <Text fontSize="detail">LinkedIn</Text>
               </Flexbox>
             </Link>
-            <Link href="https://soundcloud.com/kevin-van-leer-381908490">
+            <Link
+              color="#84878e"
+              href="https://soundcloud.com/kevin-van-leer-381908490"
+            >
               <Flexbox flexDirection="column" alignItems="center">
-                <Icon size="2x" icon={faSoundcloud} />
+                <Icon color="#ff8800" size="2x" icon={faSoundcloud} />
                 <Spacer height="0.5em" />
                 <Text fontSize="detail">SoundCloud</Text>
               </Flexbox>
             </Link>
-            <Link href="https://www.youtube.com/c/KevinVanLeerJet">
+            <Link
+              color="#84878e"
+              href="https://www.youtube.com/c/KevinVanLeerJet"
+            >
               <Flexbox flexDirection="column" alignItems="center">
-                <Icon size="2x" icon={faYoutube} />
+                <Icon size="2x" color="#f00" icon={faYoutube} />
                 <Spacer height="0.5em" />
                 <Text fontSize="detail">YouTube</Text>
               </Flexbox>
             </Link>
-            <Link href="https://www.facebook.com/kevin.vanleer">
+            <Link color="#84878e" href="https://www.facebook.com/kevin.vanleer">
               <Flexbox flexDirection="column" alignItems="center">
-                <Icon size="2x" icon={faFacebook} />
+                <Icon color="#3b5998" size="2x" icon={faFacebook} />
                 <Spacer height="0.5em" />
                 <Text fontSize="detail">Facebook</Text>
               </Flexbox>
             </Link>
-            <Link href="https://www.twitter.com/trukvl">
+            <Link color="#84878e" href="https://www.twitter.com/trukvl">
               <Flexbox flexDirection="column" alignItems="center">
-                <Icon size="2x" icon={faTwitter} />
+                <Icon color="#1da1f2" size="2x" icon={faTwitter} />
                 <Spacer height="0.5em" />
                 <Text fontSize="detail">Twitter</Text>
               </Flexbox>
